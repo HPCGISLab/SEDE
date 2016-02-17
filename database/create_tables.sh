b221b296a6118eebcf1bad4b9805268976202ad4
JayakrishnanAjayakumar <jayakrishnaneee@gmail.com>
2016-02-17 09:34:00 -0500

    Delete create_tables.sh

--- database/create_tables.sh
+++
@@ -1,5 +0,0 @@
-psql -U jajayaku -d twitter_db -a -f raw_tweet.sql
-psql -U jajayaku -d twitter_db -a -f users.sql
-psql -U jajayaku -d twitter_db -a -f tweets.sql
-psql -U jajayaku -d twitter_db -a -f user_interactions.sql
-psql -U jajayaku -d twitter_db -a -f user_mentions.sql
