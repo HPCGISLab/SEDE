12f4a6527ed02dc42066894cf3371adebe4764cb
JayakrishnanAjayakumar <jayakrishnaneee@gmail.com>
2016-03-27 11:08:07 -0400

    Hex map data holder

---
+++ UserInterface/SEDE/src/data/HexMapDataPojo.java
@@ -0,0 +1,15 @@
+package data;
+
+import java.util.ArrayList;
+
+public class HexMapDataPojo {
+    private String coordinates;
+
+	public String getCoordinates() {
+		return coordinates;
+	}
+
+	public void setCoordinates(String coordinates) {
+		this.coordinates = coordinates;
+	}
+}
