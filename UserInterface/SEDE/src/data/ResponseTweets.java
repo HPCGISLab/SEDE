518f85753e7894434426f8c08e2510829ed7b085
JayakrishnanAjayakumar <jayakrishnaneee@gmail.com>
2016-03-27 11:09:36 -0400

    Create ResponseTweets.java

---
+++ UserInterface/SEDE/src/data/ResponseTweets.java
@@ -0,0 +1,19 @@
+package data;
+
+public class ResponseTweets {
+    private long id;
+    private String body;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+    
+}
