e818f68e9c27e77a09e37d5665df1ca0025360d2
JayakrishnanAjayakumar <jayakrishnaneee@gmail.com>
2016-03-27 11:15:50 -0400

    To hold temporal data

---
+++ UserInterface/SEDE/src/data/TemporalDataPojo.java
@@ -0,0 +1,18 @@
+package data;
+
+public class TemporalDataPojo {
+    private String from;
+    private String end;
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getEnd() {
+		return end;
+	}
+	public void setEnd(String end) {
+		this.end = end;
+	}
+}
