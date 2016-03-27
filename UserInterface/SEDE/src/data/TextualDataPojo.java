36f023e3eeab88fee9d7447eceb237f3815023a5
JayakrishnanAjayakumar <jayakrishnaneee@gmail.com>
2016-03-27 11:20:09 -0400

    To hold textual data

---
+++ UserInterface/SEDE/src/data/TextualDataPojo.java
@@ -0,0 +1,15 @@
+package data;
+
+import java.util.ArrayList;
+
+public class TextualDataPojo {
+    private ArrayList<String> words;
+
+	public ArrayList<String> getWords() {
+		return words;
+	}
+
+	public void setWords(ArrayList<String> words) {
+		this.words = words;
+	}
+}
