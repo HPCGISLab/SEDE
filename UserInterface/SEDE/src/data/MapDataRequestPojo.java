9b4a804a563c5ee0ae1389ff9a7f022273d8203e
JayakrishnanAjayakumar <jayakrishnaneee@gmail.com>
2016-03-27 11:09:04 -0400

    Map data request data holder

---
+++ UserInterface/SEDE/src/data/MapDataRequestPojo.java
@@ -0,0 +1,33 @@
+package data;
+
+public class MapDataRequestPojo {
+    private SpatialDataPojo spatialdata;
+    private TemporalDataPojo temporaldata;
+    private TextualDataPojo textualdata;
+    private HexMapDataPojo hexmapcenters;
+	public SpatialDataPojo getSpatialdata() {
+		return spatialdata;
+	}
+	public TemporalDataPojo getTemporaldata() {
+		return temporaldata;
+	}
+	public TextualDataPojo getTextualdata() {
+		return textualdata;
+	}
+	
+	public HexMapDataPojo getHexmapcenters() {
+		return hexmapcenters;
+	}
+	public void setHexmapcenters(HexMapDataPojo hexmapcenters) {
+		this.hexmapcenters = hexmapcenters;
+	}
+	public void setSpatialdata(SpatialDataPojo spatialdata) {
+		this.spatialdata = spatialdata;
+	}
+	public void setTemporaldata(TemporalDataPojo temporaldata) {
+		this.temporaldata = temporaldata;
+	}
+	public void setTextualdata(TextualDataPojo textualdata) {
+		this.textualdata = textualdata;
+	}
+}
