d696f19aa0c28ae76d85e683efcba14424e3d17f
JayakrishnanAjayakumar <jayakrishnaneee@gmail.com>
2016-08-13 15:11:30 -0400

    changes to survey_response

--- database/scripts/surveyresponse.sql
+++ database/scripts/surveyresponse.sql
@@ -1,27 +1,69 @@
--- Table: public.survey_response
+--
+-- Name: survey_response; Type: TABLE; Schema: public; Owner: sede; Tablespace:
+--
 
--- DROP TABLE public.survey_response;
-
-CREATE TABLE public.survey_response
-(
-  response_id bigint NOT NULL DEFAULT nextval('survey_response_response_id_seq'::regclass),
-  survey_id bigint,
-  email character varying(100) NOT NULL,
-  resp_1 smallint,
-  resp_2 smallint,
-  resp_3 smallint,
-  resp_4 smallint,
-  resp_5 smallint,
-  tweetid bigint,
-  CONSTRAINT survey_response_pkey PRIMARY KEY (response_id),
-  CONSTRAINT survey_response_survey_id_fkey FOREIGN KEY (survey_id)
-      REFERENCES public.survey (survey_id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION,
-  CONSTRAINT survey_response_tweetid_fkey FOREIGN KEY (tweetid)
-      REFERENCES public.tweets (tweetid) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
-)
-WITH (
-  OIDS=FALSE
+CREATE TABLE survey_response (
+    response_id bigint NOT NULL,
+    survey_id bigint,
+    email character varying(100) NOT NULL,
+    resp_1 smallint,
+    resp_2 smallint,
+    resp_3 smallint,
+    resp_4 smallint,
+    resp_5 smallint,
+    tweetid bigint
 );
+
+
+ALTER TABLE survey_response OWNER TO sede;
+
+--
+-- Name: survey_response_response_id_seq; Type: SEQUENCE; Schema: public; Owner: sede
+--
+
+CREATE SEQUENCE survey_response_response_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE survey_response_response_id_seq OWNER TO sede;
+
+--
+-- Name: survey_response_response_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: sede
+--
+
+ALTER SEQUENCE survey_response_response_id_seq OWNED BY survey_response.response_id;
+
+
+--
+-- Name: response_id; Type: DEFAULT; Schema: public; Owner: sede
+--
+
+ALTER TABLE ONLY survey_response ALTER COLUMN response_id SET DEFAULT nextval('survey_response_response_id_seq'::regclass);
+
+--
+-- Name: survey_response_pkey; Type: CONSTRAINT; Schema: public; Owner: sede; Tablespace:
+--
+
+ALTER TABLE ONLY survey_response
+    ADD CONSTRAINT survey_response_pkey PRIMARY KEY (response_id);
+
+
+--
+-- Name: survey_response_survey_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: sede
+--
+
+ALTER TABLE ONLY survey_response
+    ADD CONSTRAINT survey_response_survey_id_fkey FOREIGN KEY (survey_id) REFERENCES survey(survey_id);
+
+
+--
+-- Name: survey_response_tweetid_fkey; Type: FK CONSTRAINT; Schema: public; Owner: sede
+--
+
+ALTER TABLE ONLY survey_response
+    ADD CONSTRAINT survey_response_tweetid_fkey FOREIGN KEY (tweetid) REFERENCES tweets(tweetid);
 
