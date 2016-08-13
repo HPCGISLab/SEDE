4be4d007dcb92b4f2bf2ed231610c7762a944670
JayakrishnanAjayakumar <jayakrishnaneee@gmail.com>
2016-08-13 16:37:59 -0400

    For collecting EPSG webservice request

---
+++ UserInterface/SEDE/src/data/CodePojo.java
@@ -0,0 +1,26 @@
+package data;
+
+public class CodePojo {
+	private String name;
+	private String code;
+	private String url;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+}
