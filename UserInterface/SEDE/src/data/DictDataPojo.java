95f6625f09431e749a5b95398d5ddda76e806754
JayakrishnanAjayakumar <jayakrishnaneee@gmail.com>
2016-08-13 16:38:41 -0400

    For collecting dict data

---
+++ UserInterface/SEDE/src/data/DictDataPojo.java
@@ -0,0 +1,18 @@
+package data;
+
+public class DictDataPojo {
+	private boolean used;
+	private int categid;
+	public boolean isUsed() {
+		return used;
+	}
+	public void setUsed(boolean used) {
+		this.used = used;
+	}
+	public int getCategid() {
+		return categid;
+	}
+	public void setCategid(int categid) {
+		this.categid = categid;
+	}
+}
