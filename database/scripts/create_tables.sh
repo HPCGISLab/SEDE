594b12d2c73e29d8ac859d50ff69fae825e99955
JayakrishnanAjayakumar <jayakrishnaneee@gmail.com>
2016-08-13 15:54:20 -0400

    Delete create_tables.sh

--- database/scripts/create_tables.sh
+++
@@ -1,7 +0,0 @@
-psql -U sede -d twitter_db -a -f raw_tweet.sql
-psql -U sede -d twitter_db -a -f users.sql
-psql -U sede -d twitter_db -a -f tweets.sql
-psql -U sede -d twitter_db -a -f user_interactions.sql
-psql -U sede -d twitter_db -a -f user_mentions.sql
-psql -U sede -d twitter_db -a -f survey.sql
-psql -U sede -d twitter_db -a -f surveyresponse.sql
