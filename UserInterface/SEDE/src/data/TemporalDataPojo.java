23709dd865de39f79711b10a70cfbb901614c741
JayakrishnanAjayakumar <jayakrishnaneee@gmail.com>
2016-08-13 16:35:53 -0400

    Update TemporalDataPojo.java

--- UserInterface/SEDE/src/data/TemporalDataPojo.java
+++ UserInterface/SEDE/src/data/TemporalDataPojo.java
@@ -3,16 +3,16 @@ package data;
 public class TemporalDataPojo {
     private String from;
     private String end;
-	public String getFrom() {
-		return from;
-	}
-	public void setFrom(String from) {
-		this.from = from;
-	}
-	public String getEnd() {
-		return end;
-	}
-	public void setEnd(String end) {
-		this.end = end;
-	}
+    public String getFrom() {
+        return from;
+    }
+    public void setFrom(String from) {
+	this.from = from;
+    }
+    public String getEnd() {
+	return end;
+    }
+    public void setEnd(String end) {
+        this.end = end;
+    }
 }
