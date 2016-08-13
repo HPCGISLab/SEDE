d84b5e71353fa1cbfeb52ce018568828d9027afe
JayakrishnanAjayakumar <jayakrishnaneee@gmail.com>
2016-08-13 16:24:30 -0400

    Changes for spatial request

--- UserInterface/SEDE/src/data/SpatialDataPojo.java
+++ UserInterface/SEDE/src/data/SpatialDataPojo.java
@@ -1,33 +1,10 @@
 package data;
-
 public class SpatialDataPojo {
-    private int type;
-    private String state;
-    private double coordinates[];
-    private double radius;
-	public double getRadius() {
-		return radius;
+    private String geometry;
+	public String getGeometry() {
+		return geometry;
 	}
-	public void setRadius(double radius) {
-		this.radius = radius;
+	public void setGeometry(String geometry) {
+		this.geometry = geometry;
 	}
-	public int getType() {
-		return type;
-	}
-	public void setType(int type) {
-		this.type = type;
-	}
-	public String getState() {
-		return state;
-	}
-	public void setState(String state) {
-		this.state = state;
-	}
-	public double[] getCoordinates() {
-		return coordinates;
-	}
-	public void setCoordinates(double[] coordinates) {
-		this.coordinates = coordinates;
-	}
-    
 }
